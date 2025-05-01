62a67ee455d9044bbdb96d103a7c67b33cff2661
Joe-0009 <youssefrachidi1994@gmail.com>
2025-05-01 22:46:22 +0100

    norm

--- builtins/builtins_env.c
+++ builtins/builtins_env.c
@@ -18,7 +18,23 @@ static int	is_env_var_format(char *str)
 	return (0);
 }
 
-static char	**create_temp_env(t_env *env_list, t_command *cmd, int *first_cmd_arg)
+static int	process_env_args(t_command *cmd, int *first_cmd_arg)
+{
+	int	extra_vars;
+
+	*first_cmd_arg = 1;
+	extra_vars = 0;
+	while (*first_cmd_arg < cmd->args_count
+		&& is_env_var_format(cmd->args[*first_cmd_arg]))
+	{
+		(*first_cmd_arg)++;
+		extra_vars++;
+	}
+	return (extra_vars);
+}
+
+static char	**setup_and_copy_env(t_env *env_list, t_command *cmd, 
+		int *first_cmd_arg)
 {
 	char	**env_array;
 	char	**temp_env;
@@ -27,42 +43,22 @@ static char	**create_temp_env(t_env *env_list, t_command *cmd, int *first_cmd_ar
 	int		i;
 	int		j;
 
-	*first_cmd_arg = 1;
-	extra_vars = 0;
-	while (*first_cmd_arg < cmd->args_count 
-		&& is_env_var_format(cmd->args[*first_cmd_arg]))
-	{
-		(*first_cmd_arg)++;
-		extra_vars++;
-	}
-	
+	extra_vars = process_env_args(cmd, first_cmd_arg);
 	env_array = env_list_to_envp(env_list);
 	if (!env_array)
 		return (NULL);
-	
 	env_count = 0;
 	while (env_array[env_count])
 		env_count++;
-		
 	temp_env = ft_calloc(env_count + extra_vars + 1, sizeof(char *));
 	if (!temp_env)
-	{
-		safe_doube_star_free(env_array);
-		return (NULL);
-	}
-	
-	i = 0;
-	while (env_array[i])
-	{
+		return (safe_doube_star_free(env_array), NULL);
+	i = -1;
+	while (env_array[++i])
 		temp_env[i] = ft_strdup(env_array[i]);
-		i++;
-	}
-	
 	j = 1;
 	while (j < *first_cmd_arg)
 		temp_env[i++] = ft_strdup(cmd->args[j++]);
-	
-	temp_env[i] = NULL;
 	safe_doube_star_free(env_array);
 	return (temp_env);
 }
@@ -71,10 +67,9 @@ static int	handle_cmd_exec(t_command *cmd, t_env *env_list, int first_cmd_arg)
 {
 	char	**temp_env;
 
-	temp_env = create_temp_env(env_list, cmd, &first_cmd_arg);
+	temp_env = setup_and_copy_env(env_list, cmd, &first_cmd_arg);
 	if (!temp_env)
 		return (1);
-	
 	if (access(cmd->args[first_cmd_arg], F_OK) == -1)
 	{
 		ft_putstr_fd("env: ", 2);
--- utils/fprintf_utils.c
+++ utils/fprintf_utils.c
@@ -1,36 +1,32 @@
 #include "../minishell.h"
 
-static int	append_string(char **result, const char *str)
+static int	append_to_result(char **result, const char *str, char c, int is_char)
 {
 	char	*temp;
-	int		len;
+	size_t		(curr_len), (len);
 
-	if (!str)
-		str = "(null)";
-	len = ft_strlen(str);
-	temp = *result;
-	*result = ft_strjoin(*result, str);
-	free(temp);
-	return (len);
-}
-
-static int	append_char(char **result, char c)
-{
-	char	*temp;
-	size_t	curr_len;
-
-	curr_len = ft_strlen(*result);
-	temp = *result;
-	*result = ft_calloc(curr_len + 2, sizeof(char));
-	if (!*result)
+	if (is_char)
 	{
+		curr_len = ft_strlen(*result);
+		temp = *result;
+		*result = ft_calloc(curr_len + 2, sizeof(char));
+		if (!*result)
+			return (free(temp), -1);
+		ft_strlcpy(*result, temp, curr_len + 1);
+		(*result)[curr_len] = c;
 		free(temp);
-		return (-1);
+		return (1);
 	}
-	ft_strlcpy(*result, temp, curr_len + 1);
-	(*result)[curr_len] = c;
-	free(temp);
-	return (1);
+	else
+	{
+		if (!str)
+			str = "(null)";
+		len = ft_strlen(str);
+		temp = *result;
+		*result = ft_strjoin(*result, str);
+		free(temp);
+		return (len);
+	}
 }
 
 static int	process_format_specifier(char **result, char specifier,
@@ -42,44 +38,38 @@ static int	process_format_specifier(char **result, char specifier,
 	if (specifier == 's')
 	{
 		str = va_arg(args, char *);
-		return (append_string(result, str));
+		return (append_to_result(result, str, 0, 0));
 	}
 	else if (specifier == 'c')
 	{
 		c = (char)va_arg(args, int);
-		return (append_char(result, c));
+		return (append_to_result(result, NULL, c, 1));
 	}
-	if (append_char(result, '%') == -1 || append_char(result, specifier) == -1)
+	if (append_to_result(result, NULL, '%', 1) == -1 || 
+		append_to_result(result, NULL, specifier, 1) == -1)
 		return (-1);
 	return (2);
 }
 
-static int	process_format_char(char **result, const char **format, int *i,
+static int	process_format_char(char **result, const char *format, int *i,
 		va_list args)
 {
 	int	ret;
 
-	if ((*format)[*i] == '%' && (*format)[*i + 1])
+	if (format[*i] == '%' && format[*i + 1])
 	{
 		(*i)++;
-		ret = process_format_specifier(result, (*format)[*i], args);
+		ret = process_format_specifier(result, format[*i], args);
 		if (ret == -1)
 			return (-1);
 		return (ret);
 	}
 	else
 	{
-		if (append_char(result, (*format)[*i]) == -1)
+		if (append_to_result(result, NULL, format[*i], 1) == -1)
 			return (-1);
 		return (1);
 	}
-}
-
-static int	cleanup_fprintf(char *result, va_list args)
-{
-	free(result);
-	va_end(args);
-	return (-1);
 }
 
 static int	process_format_string(char **result, const char *format,
@@ -93,9 +83,13 @@ static int	process_format_string(char **result, const char *format,
 	i = 0;
 	while (format[i])
 	{
-		ret = process_format_char(result, &format, &i, args);
+		ret = process_format_char(result, format, &i, args);
 		if (ret == -1)
+		{
+			free(*result);
+			va_end(args);
 			return (-1);
+		}
 		count += ret;
 		i++;
 	}
@@ -114,7 +108,7 @@ int	ft_fprintf_fd(int fd, const char *format, ...)
 		return (-1);
 	count = process_format_string(&result, format, args);
 	if (count == -1)
-		return (cleanup_fprintf(result, args));
+		return (-1);
 	ft_putstr_fd(result, fd);
 	free(result);
 	va_end(args);
