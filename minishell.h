6f08db3f14199d8e0b7cd0fd581df23e4b976b0e
Joe-0009 <youssefrachidi1994@gmail.com>
2025-05-02 22:45:10 +0100

    norms all day

--- minishell.h
+++ minishell.h
@@ -9,6 +9,7 @@
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
+# include <stdarg.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
@@ -17,9 +18,8 @@
 # include <sys/wait.h>
 # include <termios.h>
 # include <unistd.h>
-# include <stdarg.h>
 
-extern int	g_last_exit_status;
+extern int					g_last_exit_status;
 
 /**
  * Parser state for handling quotes
@@ -29,7 +29,7 @@ typedef enum e_state
 	STATE_NORMAL,
 	STATE_IN_SINGLE_QUOTE,
 	STATE_IN_DOUBLE_QUOTE
-}	t_state;
+}							t_state;
 
 /**
  * Token types for parsing
@@ -42,7 +42,7 @@ typedef enum e_token_type
 	TOKEN_REDIRECT_IN,
 	TOKEN_APPEND,
 	TOKEN_HEREDOC
-}	t_token_type;
+}							t_token_type;
 
 /**
  * Token structure for lexical analysis
@@ -52,7 +52,7 @@ typedef struct s_token
 	char					*content;
 	t_token_type			type;
 	struct s_token			*next;
-}	t_token;
+}							t_token;
 
 /**
  * Redirection structure for I/O operations
@@ -63,7 +63,7 @@ typedef struct s_redirections
 	char					*file;
 	int						heredoc_fd;
 	struct s_redirections	*next;
-}	t_redirections;
+}							t_redirections;
 
 /**
  * Command structure representing a single command or pipeline element
@@ -74,7 +74,7 @@ typedef struct s_command
 	int						args_count;
 	t_redirections			*redirections;
 	struct s_command		*next;
-}	t_command;
+}							t_command;
 
 /**
  * Environment variable linked list structure
@@ -84,7 +84,7 @@ typedef struct s_env
 	char					*key;
 	char					*value;
 	struct s_env			*next;
-}	t_env;
+}							t_env;
 
 /* ===================== MEMORY MANAGEMENT ===================== */
 void						free_command(t_command *cmd);
@@ -106,21 +106,21 @@ int							is_content_quoted(char *content);
 /* ===================== TOKENIZER ===================== */
 typedef struct s_tokenizer
 {
-	t_token					**tokens;     /* List of tokens being built */
-	char					*input;       /* Input string being tokenized */
-	int						i;            /* Current position in input */
-	int						start;        /* Start position of current token */
-	t_state					state;        /* Current quote state */
-}	t_tokenizer;
+	t_token **tokens; /* List of tokens being built */
+	char *input;      /* Input string being tokenized */
+	int i;            /* Current position in input */
+	int start;        /* Start position of current token */
+	t_state state;    /* Current quote state */
+}							t_tokenizer;
 
 typedef struct s_quote_ctx
 {
-	char					c;            /* Current character being processed */
-	size_t					*i;           /* Current position pointer */
-	int						*in_quotes;   /* Whether we're in quotes */
-	int						*quoted;      /* Whether string has quotes */
-	char					*quote_type;  /* Current quote type (' or ") */
-}	t_quote_ctx;
+	char c;           /* Current character being processed */
+	size_t *i;        /* Current position pointer */
+	int *in_quotes;   /* Whether we're in quotes */
+	int *quoted;      /* Whether string has quotes */
+	char *quote_type; /* Current quote type (' or ") */
+}							t_quote_ctx;
 
 t_token						*tokenize_input(char *input);
 void						assign_token_types(t_token *tokens);
@@ -159,18 +159,19 @@ int							setup_redirect_out(char *file_path,
 								int append_mode);
 int							handle_redirect_token(t_token **current,
 								t_command **first_cmd, t_command **current_cmd);
-void						add_redirection(t_command *cmd, t_token_type redirect_type, char *file);
+void						add_redirection(t_command *cmd,
+								t_token_type redirect_type, char *file);
 
 /* ===================== BUILTINS ===================== */
 typedef struct s_env_setup
 {
-	char					**env_array;       /* Array of environment variables */
-	char					**temp_env;        /* Temporary environment array */
-	int						env_count;         /* Count of environment variables */
-	int						extra_vars;        /* Count of extra variables */
-	int						i;                 /* Loop counter */
-	int						j;                 /* Loop counter */
-}	t_env_setup;
+	char **env_array; /* Array of environment variables */
+	char **temp_env;  /* Temporary environment array */
+	int env_count;    /* Count of environment variables */
+	int extra_vars;   /* Count of extra variables */
+	int i;            /* Loop counter */
+	int j;            /* Loop counter */
+}							t_env_setup;
 
 int							is_builtin_command(char *cmd);
 int							execute_builtin(t_command *cmd, t_env **env_list);
@@ -187,8 +188,8 @@ void						print_environment(t_env *env_list, t_command *cmd);
 
 /* ===================== ENV LINKED LIST HELPERS ===================== */
 t_env						*find_env_node(t_env *env_list, const char *key);
-void						add_or_update_env(t_env **env_list,
-								const char *key, const char *value);
+void						add_or_update_env(t_env **env_list, const char *key,
+								const char *value);
 void						print_export_list(t_env *env_list);
 t_env						*envp_to_env_list(char **envp);
 char						**env_list_to_envp(t_env *env_list);
@@ -215,22 +216,23 @@ void						expand_command_args(t_command *cmd, char **envp);
 /* ===================== ENV EXPANSION ARGS UTILS ===================== */
 typedef struct s_expand_vars
 {
-	t_command				*cmd;          /* Command being processed */
-	char					**envp;        /* Environment variables */
-	int						i;             /* Current argument index */
-	int						j;             /* Output argument index */
-	int						added;         /* Number of args added by splitting */
-	int						is_export;     /* Whether this is an export command */
-	int						was_arg_quoted; /* Whether argument had quotes */
-	int						had_quoted_vars; /* Whether argument had quoted variables */
-}	t_expand_vars;
+	t_command *cmd;      /* Command being processed */
+	char **envp;         /* Environment variables */
+	int i;               /* Current argument index */
+	int j;               /* Output argument index */
+	int added;           /* Number of args added by splitting */
+	int is_export;       /* Whether this is an export command */
+	int was_arg_quoted;  /* Whether argument had quotes */
+	int had_quoted_vars; /* Whether argument had quoted variables */
+}							t_expand_vars;
 
 int							count_split_words(char **split_words);
-void						copy_and_replace_args(t_command *cmd, char **new_args,
-								int pos, char **split_words);
+void						copy_and_replace_args(t_command *cmd,
+								char **new_args, int pos, char **split_words);
 int							add_split_args_to_command(t_command *cmd, int pos,
 								char **split_words);
-void						expand_and_strip_arg(t_command *cmd, char **envp, int i);
+void						expand_and_strip_arg(t_command *cmd, char **envp,
+								int i);
 int							split_and_insert_args(t_expand_vars *v);
 void						compact_args(t_command *cmd, int *i, int *j);
 void						expand_args_loop(t_expand_vars *v);
@@ -241,7 +243,8 @@ int							was_quoted(const char *str);
 
 /* ===================== EXECUTOR UTILS ===================== */
 int							setup_pipe(int pipe_fd[2]);
-int							handle_heredoc_redir(t_redirections *redir, char **envp);
+int							handle_heredoc_redir(t_redirections *redir,
+								char **envp);
 void						handle_fork_error(t_command *current,
 								int prev_pipe_read, int pipe_fd[2]);
 
@@ -256,7 +259,8 @@ int							parent_process(int prev_pipe_read, int pipe_fd[2]);
 int							setup_command_pipe(t_command *current,
 								int *prev_pipe_read, int pipe_fd[2]);
 int							is_parent_builtin(char *cmd);
-int							execute_single_parent_builtin(t_command *cmd_list, t_env **env_list);
+int							execute_single_parent_builtin(t_command *cmd_list,
+								t_env **env_list);
 int							print_env_vars(char **envp);
 void						handle_builtin_command(t_command *current,
 								t_env *env_list);
